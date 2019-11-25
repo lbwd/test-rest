4c82f25243220d1e6238e65128f0f53acb7f44c2
Luca Benedetti <luca.benedetti@euei.it>
2019-11-25 12:31:51 +0100

    First commit

---
+++ testRest.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var app = http.createServer(function(req, res) {
+  console.log(req.url);
+  var body = [];
+  req
+    .on('data', function(chunk) {
+      body.push(chunk);
+    })
+    .on('end', function() {
+      body = Buffer.concat(body).toString();
+      if (body) console.log(JSON.parse(body));
+      res.end();
+    });
+});
+app.listen(3000);
