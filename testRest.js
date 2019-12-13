5339ce02366893d1434de8b71074d0f17313586e
Luca Benedetti <luca.benedetti@euei.it>
2019-12-13 16:55:29 +0100

    Replace newline.

--- testRest.js
+++ testRest.js
@@ -13,7 +13,7 @@ var app = http.createServer(function(req, res) {
         console.log(
           "curl --user \"***CREDENTIALS***\" -X POST 'http://127.0.0.1:8080/mgn-services/addevent' -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d \"" +
             body +
-            '"'.trim()
+            '"'.trim().replace(/(\r\n|\n|\r)/gm, '')
         );
       }
       res.end();
